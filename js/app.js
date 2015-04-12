55316696839d591be10894c546c01afb44f10721
Jared Kerschner <jhkerschner@gmail.com>
2015-04-12 10:48:44 -0400

    Changed the way the hole data saves, and how the leader board displays it. Each hole score will have a TD now.

--- js/app.js
+++ js/app.js
@@ -11,7 +11,8 @@ myApp.controller('player', ['$scope', '$firebase', '$location',
     
     $scope.save = function() {
       if($scope.item.player != null){
-        sync.$push({ player:$scope.item.player, total:0 }).then(function(ref) {
+        holes = {'h1':0,'h2':0,'h3':0,'h4':0,'h5':0,'h6':0,'h7':0,'h8':0,'h9':0,'h10':0,'h11':0,'h12':0,'h13':0,'h14':0,'h15':0,'h16':0,'h17':0,'h18':0};
+        sync.$push({ player:$scope.item.player, total:0, holes:holes }).then(function(ref) {
           userID = ref.key();
           $location.path('/scorecard/hole/'+currHole+'/');
         });
@@ -24,7 +25,7 @@ myApp.controller('scorecard', ['$scope', '$firebase', '$location',
   function($scope, $firebase, $location) {
     //CREATE A FIREBASE REFERENCE
     var ref = new Firebase("https://ema-masters.firebaseio.com/players/"+userID+"/holes/");
-    var sync = $firebase(ref).$asArray();
+    var sync = $firebase(ref).$asObject();
 
     var ref_total_score = new Firebase("https://ema-masters.firebaseio.com/players/"+userID+"/");
     var sync_total_score = $firebase(ref_total_score).$asObject();
@@ -38,7 +39,67 @@ myApp.controller('scorecard', ['$scope', '$firebase', '$location',
         sync_total_score.total = totalScore;
         sync_total_score.$save();
 
-        sync.$add({ hole:currHole, score:$scope.item.score }).then(function(ref) {
+        //objectHole = 'h'+currHole;
+
+        switch(currHole) {
+          case 1:
+              sync.h1 = $scope.item.score;
+              break;
+          case 2:
+              sync.h2 = $scope.item.score;
+              break;
+          case 3:
+              sync.h3 = $scope.item.score;
+              break;
+          case 4:
+              sync.h4 = $scope.item.score;
+              break;
+          case 5:
+              sync.h5 = $scope.item.score;
+              break;
+          case 6:
+              sync.h6 = $scope.item.score;
+              break;
+          case 7:
+              sync.h7 = $scope.item.score;
+              break;
+          case 8:
+              sync.h8 = $scope.item.score;
+              break;
+          case 9:
+              sync.h9 = $scope.item.score;
+              break;
+          case 10:
+              sync.h10 = $scope.item.score;
+              break;
+          case 11:
+              sync.h11 = $scope.item.score;
+              break;
+          case 12:
+              sync.h12 = $scope.item.score;
+              break;
+          case 13:
+              sync.h13 = $scope.item.score;
+              break;
+          case 14:
+              sync.h14 = $scope.item.score;
+              break;
+          case 15:
+              sync.h15 = $scope.item.score;
+              break;
+          case 16:
+              sync.h16 = $scope.item.score;
+              break;
+          case 17:
+              sync.h17 = $scope.item.score;
+              break;
+          case 18:
+              sync.h18 = $scope.item.score;
+              break;
+         
+      }
+
+        sync.$save().then(function(ref) {
           if(next == 19){
            $location.path('/round-complete/');
           }else{
@@ -46,7 +107,26 @@ myApp.controller('scorecard', ['$scope', '$firebase', '$location',
           }
           currHole++;
         });
-      }
+        /*sync.h1.$save(function(){
+          if(next == 19){
+           $location.path('/round-complete/');
+          }else{
+            $location.path('/scorecard/hole/'+(next)+'/');
+          }
+          currHole++;
+        });*/
+
+
+        }
+        /*sync.$add({ hole:currHole, score:$scope.item.score }).then(function(ref) {
+          if(next == 19){
+           $location.path('/round-complete/');
+          }else{
+            $location.path('/scorecard/hole/'+(next)+'/');
+          }
+          currHole++;
+        });
+      }*/
       
     };
   }
